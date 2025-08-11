0ac72de47874813226a2cbbed66def7409d66091
ShamirAli55 <shamirali9779@gmail.com>
2025-08-11 02:46:54 -0700

    Second commit

--- 01/script.js
+++
@@ -1,5 +0,0 @@
-gsap.to("#box",{
-    x : 900,
-    duration : 2,
-    delay:5
-})
---
+++ Basics/script.js
@@ -0,0 +1,46 @@
+// gsap.to("#box",{
+//     x : 900,
+//     duration : 2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"50%",
+//     scale:0.5
+// })
+// gsap.from("#box1",{
+//     x : 900,
+//     duration : 2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"50%",
+//     scale:0.5
+// })
+
+// <-- Stagger -->
+
+// gsap.from("h1",{
+//     opacity:0,
+//     duration : 2,
+//     delay:0.3,
+//     y:40,
+//     stagger:0.5,
+//     Color:"black"
+// })
+
+
+// <--- Repeat & Yoyo --->
+
+// gsap.to("#box",{
+//     x:700,
+//     y:20,
+//     duration:2,
+//     delay:1,
+//     rotate:90,
+//     repeat:-1,
+//     yoyo:true
+// })
+
+
+
+

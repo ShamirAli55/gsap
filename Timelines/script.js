ade868109203ac24fb3382867ebfe9584ea4d514
ShamirAli55 <shamirali9779@gmail.com>
2025-08-11 03:51:19 -0700

    second commit

--- Timelines/script.js
+++ Timelines/script.js
@@ -1,1 +1,30 @@
 // <--- Timeline --->
+var tl = gsap.timeline();
+
+tl.to("#box1",{
+    x : 900,
+    duration : 2,
+    delay:0.5,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.5
+})
+tl.to("#box2",{
+    x : 900,
+    duration : 2,
+    delay:0.5,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.5
+})
+tl.to("#box3",{
+    x : 900,
+    duration : 2,
+    delay:0.5,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.5
+})
